37398863a86454851be2c79b3682aae95d757cc5
Flouis2017 <315587951@qq.com>
2019-04-07 22:55:46 +0800

    init part of admin pages

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,7 +1,37 @@
 # coding:utf8
 from . import admin
+from flask import render_template, redirect, url_for
 
 
 @admin.route("/")
 def index():
-    return "<h1 style='color:red'>this is admin</h1>"
+    return render_template("admin/index.html")
+
+
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.login"))
+
+
+@admin.route("/pwd/")
+def pwd():
+    return render_template("admin/pwd.html")
+
+
+@admin.route("/tag_add/")
+def tag_add():
+    return render_template("admin/tag_add.html")
+
+
+@admin.route("/tag_list/")
+def tag_list():
+    return render_template("admin/tag_list.html")
+
+
+
+
