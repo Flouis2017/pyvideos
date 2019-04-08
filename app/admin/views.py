5defa20576a9c9c0191ae3a1f3ebbd88ccbf6b38
Flouis2017 <315587951@qq.com>
2019-04-09 00:51:17 +0800

    init admin pages

--- app/admin/views.py
+++ app/admin/views.py
@@ -33,5 +33,91 @@ def tag_list():
     return render_template("admin/tag_list.html")
 
 
+@admin.route("/video_add/")
+def video_add():
+    return render_template("admin/video_add.html")
+
+
+@admin.route("/video_list/")
+def video_list():
+    return render_template("admin/video_list.html")
+
+
+@admin.route("/preview_add/")
+def preview_add():
+    return render_template("admin/preview_add.html")
+
+
+@admin.route("/preview_list/")
+def preview_list():
+    return render_template("admin/preview_list.html")
+
+
+@admin.route("/user_list/")
+def user_list():
+    return render_template("admin/user_list.html")
+
+
+@admin.route("/user_view/")
+def user_view():
+    return render_template("admin/user_view.html")
+
+
+@admin.route("/comment_list/")
+def comment_list():
+    return render_template("admin/comment_list.html")
+
+
+@admin.route("/collection_list/")
+def collection_list():
+    return render_template("admin/collection_list.html")
+
+
+@admin.route("/admin_op_log_list/")
+def admin_op_log_list():
+    return render_template("admin/admin_op_log_list.html")
+
+
+@admin.route("/admin_login_log_list/")
+def admin_login_log_list():
+    return render_template("admin/admin_login_log_list.html")
+
+
+@admin.route("/login_log_list/")
+def login_log_list():
+    return render_template("admin/login_log_list.html")
+
+
+@admin.route("/auth_add/")
+def auth_add():
+    return render_template("admin/auth_add.html")
+
+
+@admin.route("/auth_list/")
+def auth_list():
+    return render_template("admin/auth_list.html")
+
+
+@admin.route("/role_add/")
+def role_add():
+    return render_template("admin/role_add.html")
+
+
+@admin.route("/role_list/")
+def role_list():
+    return render_template("admin/role_list.html")
+
+
+@admin.route("/admin_user_add/")
+def admin_user_add():
+    return render_template("admin/admin_user_add.html")
+
+
+@admin.route("/admin_user_list/")
+def admin_user_list():
+    return render_template("admin/admin_user_list.html")
+
+
+
 
 
