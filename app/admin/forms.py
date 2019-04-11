9c153e050a77c0db9a7c2c05b2ffae7169ebb981
Flouis2017 <315587951@qq.com>
2019-04-12 01:09:45 +0800

    admin tag_add & tag_list

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -5,9 +5,9 @@ from wtforms.validators import DataRequired, ValidationError
 from app.models import AdminUser
 
 
-# 管理员登录(验证)表单,定义完表单后，在前端使用{{ <属性名> }}这种方式去填充界面后，flask会帮我们做好表单验证
 # StringField对应的前端input元素type是text
 # PasswordField对应的前端input元素type是password
+# 管理员登录(验证)表单,定义完表单后，在前端使用{{ <属性名> }}这种方式去填充界面后，flask会帮我们做好表单验证
 class LoginForm(FlaskForm):
     username = StringField(
         label="用户名",
@@ -40,6 +40,28 @@ class LoginForm(FlaskForm):
     def validate_username(self, field):
         if AdminUser.query.filter_by(username=field.data).count() == 0:
             raise ValidationError("用户不存在！")
+
+
+# 标签表单
+class TagForm(FlaskForm):
+    name = StringField(
+        label="标签名称",
+        validators=[
+            DataRequired("请输入标签名称！")
+        ],
+        description="标签名称",
+        render_kw={
+            "class": "form-control",
+            "id": "input_name"
+        }
+    )
+    submit = SubmitField(
+        "添加",
+        render_kw={
+            "class": "btn btn-primary"
+        }
+    )
+
 
 
 
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,9 +1,10 @@
 # coding:utf8
 from . import admin
-from flask import render_template, redirect, url_for, flash, session, request
-from app.admin.forms import LoginForm
-from app.models import AdminUser
+from flask import render_template, redirect, url_for, flash, session, request, jsonify
+from app.admin.forms import *
+from app.models import *
 from functools import wraps
+from app import db
 
 
 # 登录拦截器(注意装饰器的写法)
@@ -23,6 +24,7 @@ def index():
     return render_template("admin/index.html")
 
 
+# 登录
 @admin.route("/login/", methods=["GET", "POST"])
 def login():
     form = LoginForm()
@@ -38,128 +40,166 @@ def login():
     return render_template("admin/login.html", form=form)
 
 
-@admin.route("/logout/")
+# 退出
+@admin.route("/logout/", methods=["GET", "POST"])
 @admin_login_req
 def logout():
     session.pop("username", None)
     return redirect(url_for("admin.login"))
 
 
-@admin.route("/pwd/")
+# 修改密码
+@admin.route("/pwd/", methods=["GET", "POST"])
 @admin_login_req
 def pwd():
     return render_template("admin/pwd.html")
 
 
-@admin.route("/tag_add/")
+# 添加标签
+@admin.route("/tag_add/", methods=["GET", "POST"])
 @admin_login_req
 def tag_add():
-    return render_template("admin/tag_add.html")
+    form = TagForm()
+    if form.validate_on_submit():
+        data = form.data
+        # 重复性校验
+        cnt = Tag.query.filter_by(name=data["name"]).count()
+        if cnt == 1:
+            flash("已存在！", "err")
+            return redirect(url_for("admin.tag_add"))
+        tag = Tag(
+            name=data["name"]
+        )
+        db.session.add(tag)
+        db.session.commit()
+        flash("操作成功", "ok")
+        return redirect(url_for("admin.tag_add"))
+    return render_template("admin/tag_add.html", form=form)
 
 
+# 标签列表（路由默认处理GET请求）
 @admin.route("/tag_list/")
 @admin_login_req
 def tag_list():
-    return render_template("admin/tag_list.html")
+    page = int(request.args.get("page", "1"))
+    size = int(request.args.get("size", "1"))
+    page_data = Tag.query.order_by(
+        Tag.create_time.desc()
+    ).paginate(page=page, per_page=size)
+    return render_template("admin/tag_list.html", page_data=page_data)
 
 
-@admin.route("/video_add/")
+# 测试ajax交互
+@admin.route("/test_ajax", methods=["GET", "POST"])
+@admin_login_req
+def test_ajax():
+    form = request.form
+    print(form)
+    name = form.get("name")
+    age = form.get("age")
+    gender = form.get("gender")
+    print(name, age, gender)
+    json_resp = {"code": 200, "msg": "Ajax Success!", "data": None}
+    return jsonify(json_resp)
+
+
+@admin.route("/video_add/", methods=["GET", "POST"])
 @admin_login_req
 def video_add():
     return render_template("admin/video_add.html")
 
 
-@admin.route("/video_list/")
+@admin.route("/video_list/", methods=["GET", "POST"])
 @admin_login_req
 def video_list():
     return render_template("admin/video_list.html")
 
 
-@admin.route("/preview_add/")
+@admin.route("/preview_add/", methods=["GET", "POST"])
 @admin_login_req
 def preview_add():
     return render_template("admin/preview_add.html")
 
 
-@admin.route("/preview_list/")
+@admin.route("/preview_list/", methods=["GET", "POST"])
 @admin_login_req
 def preview_list():
     return render_template("admin/preview_list.html")
 
 
-@admin.route("/user_list/")
+@admin.route("/user_list/", methods=["GET", "POST"])
 @admin_login_req
 def user_list():
     return render_template("admin/user_list.html")
 
 
-@admin.route("/user_view/")
+@admin.route("/user_view/", methods=["GET", "POST"])
 @admin_login_req
 def user_view():
     return render_template("admin/user_view.html")
 
 
-@admin.route("/comment_list/")
+@admin.route("/comment_list/", methods=["GET", "POST"])
 @admin_login_req
 def comment_list():
     return render_template("admin/comment_list.html")
 
 
-@admin.route("/collection_list/")
+@admin.route("/collection_list/", methods=["GET", "POST"])
 @admin_login_req
 def collection_list():
     return render_template("admin/collection_list.html")
 
 
-@admin.route("/admin_op_log_list/")
+@admin.route("/admin_op_log_list/", methods=["GET", "POST"])
 @admin_login_req
 def admin_op_log_list():
     return render_template("admin/admin_op_log_list.html")
 
 
-@admin.route("/admin_login_log_list/")
+@admin.route("/admin_login_log_list/", methods=["GET", "POST"])
 @admin_login_req
 def admin_login_log_list():
     return render_template("admin/admin_login_log_list.html")
 
 
-@admin.route("/login_log_list/")
+@admin.route("/login_log_list/", methods=["GET", "POST"])
 @admin_login_req
 def login_log_list():
     return render_template("admin/login_log_list.html")
 
 
-@admin.route("/auth_add/")
+@admin.route("/auth_add/", methods=["GET", "POST"])
 @admin_login_req
 def auth_add():
     return render_template("admin/auth_add.html")
 
 
-@admin.route("/auth_list/")
+@admin.route("/auth_list/", methods=["GET", "POST"])
 @admin_login_req
 def auth_list():
     return render_template("admin/auth_list.html")
 
 
-@admin.route("/role_add/")
+@admin.route("/role_add/", methods=["GET", "POST"])
 @admin_login_req
 def role_add():
     return render_template("admin/role_add.html")
 
 
-@admin.route("/role_list/")
+@admin.route("/role_list/", methods=["GET", "POST"])
 @admin_login_req
 def role_list():
     return render_template("admin/role_list.html")
 
 
-@admin.route("/admin_user_add/")
+@admin.route("/admin_user_add/", methods=["GET", "POST"])
 @admin_login_req
 def admin_user_add():
     return render_template("admin/admin_user_add.html")
 
 
-@admin.route("/admin_user_list/")
+@admin.route("/admin_user_list/", methods=["GET", "POST"])
 @admin_login_req
 def admin_user_list():
     return render_template("admin/admin_user_list.html")
